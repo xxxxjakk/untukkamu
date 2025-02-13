e4b2f010e30f2d0db267833dca70fd5808cc2eeb
xxxxjakk <zaks1008@gmail.com>
2025-02-13 15:59:36 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const messages = [
+    "Are you sure?",
+    "Really sure??",
+    "Are you positive?",
+    "Pookie please...",
+    "Just think about it!",
+    "If you say no, I will be really sad...",
+    "I will be very sad...",
+    "I will be very very very sad...",
+    "Ok fine, I will stop asking...",
+    "Just kidding, say yes please! ❤️"
+];
+
+let messageIndex = 0;
+
+function handleNoClick() {
+    const noButton = document.querySelector('.no-button');
+    const yesButton = document.querySelector('.yes-button');
+    noButton.textContent = messages[messageIndex];
+    messageIndex = (messageIndex + 1) % messages.length;
+    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+}
+
+function handleYesClick() {
+    window.location.href = "yes_page.html";
+}
+
+function handleTestClick() {
+    window.location.href = "click_this.html";
+}
+
+
+// Musik akan otomatis dimainkan, pastikan browser mendukung autoplay
+document.addEventListener("DOMContentLoaded", function() {
+    var music = document.getElementById("bg-music");
+    music.volume = 0.5; // Atur volume musik agar tidak terlalu keras
+    music.play();
+});
